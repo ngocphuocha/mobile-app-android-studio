a5afae257726b7be8d482ea70cf8fbaf94913cbf
ngocphuocha <phuoc04012000@gmail.com>
2022-08-03 15:29:33 +0700

    init

---
+++ app/src/main/java/com/example/com1786tcd0401firstapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.com1786tcd0401firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView helloText = findViewById(R.id.textHello);
+
+        TextView markPoint = findViewById(R.id.markPoint);
+        helloText.setText("Hello world");
+
+        int mark = 0;
+
+        Random r = new Random();
+        mark = r.nextInt(100);
+        mark += 1;
+
+        markPoint.setText("" + mark); // Display mark point
+
+        if (mark < 50) {
+            helloText.setText("Sorry you failed");
+            helloText.setTextColor(Color.RED);
+        } else if (mark > 70) {
+            helloText.setText("Brilliant, you got a distinction");
+            helloText.setTextColor(Color.parseColor("#239B56"));
+        } else {
+            helloText.setText("Well done you passed");
+            helloText.setTextColor(Color.GREEN);
+        }
+    }
+}
