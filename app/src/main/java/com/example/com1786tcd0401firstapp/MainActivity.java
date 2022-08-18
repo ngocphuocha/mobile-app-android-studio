466499608d91426b47d43c58ecea2565ac1ffd57
ngocphuocha <phuoc04012000@gmail.com>
2022-08-18 18:14:27 +0700

    List Item Weekend

--- app/src/main/java/com/example/com1786tcd0401firstapp/MainActivity.java
+++ app/src/main/java/com/example/com1786tcd0401firstapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.com1786tcd0401firstapp;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends ListActivity {
+    private static final String[] WEEKDAYS = new String[]{
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+    };
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, WEEKDAYS));
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long ig) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
